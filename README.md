52a55b916482ed1dae57919f71e772b9ce23fb7e
Darshika-25 <darshikaparekh25@gmail.com>
2024-07-10 17:37:21 +0500

    project

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a CLI number guessing game made with typescript and node.js using Inquirer library.
+The user guesses a number between 1 and 6. Meanwhile, awaits the victory or defeat via computer generated number.
+
+PLAY YOURSELF
+npx @darshika.dev/number_guessing_game
---
+++ game.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+//shebang for npm
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// 1) Computer will generate a random Number
+// 2) User input for Gussing a Number
+// 3) Compare both numbers and show results
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please Guess a Number between 1 - 6",
+  },
+]);
+
+if (answers.userGuessedNumber === randomNumber) 
+{console.log (chalk.bold.blueBright("Congratulations! you guessed the right number."));}
+else {console.log (chalk.bold.magenta(`You guessed the wrong number, the correct answer is ${randomNumber}`))};
